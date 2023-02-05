07418ca27651dc368bc2441e1d88ddc5bff55e2f
nguyenthonam <12978437+nguyenthonam@users.noreply.github.com>
2023-02-05 12:39:40 +0700

    Done setup call api

--- src/pages/home/view/index.vue
+++ src/pages/home/view/index.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-import { fetchBanners } from "@/apis/banners/fetchBanners";
+import useApiBanners from "@/apis/banners/useApiBanners";
+import { watch, isRef } from "vue-demi";
+const { fetchBanners } = useApiBanners();
 const { isLoading, isError, data, error, isFetching } = fetchBanners();
-console.log("Home Banner :>> ", data);
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Home Page</h1>
     <p>Welcome <span class="text-primary">Nam Nguyen!</span></p>
+    ==================================
+    <div>
+      <span v-if="isLoading">Loading...</span>
+      <span v-else-if="isError">Error: {{ error }}</span>
+      <!-- We can assume by this point that `isSuccess === true` -->
+      <ul v-else>
+        <br />
+        --------
+        <li v-for="(item, idx) in data" :key="'todo' + idx">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
